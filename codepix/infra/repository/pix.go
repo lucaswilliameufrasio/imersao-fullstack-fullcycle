5ad66e49fb46b3761a0ac38a38d3cacafd415576
Lucas William <lucaswilliameufrasio@gmail.com>
2021-02-04 02:36:35 -0300

    feat: add PixKeyRepositoryDB

---
+++ codepix/infra/repository/pix.go
@@ -0,0 +1,66 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	"github.com/lucaswilliameufrasio/imersao/codepix-go/domain/model"
+)
+
+type PixKeyRepositoryDB struct {
+	DB *gorm.DB
+}
+
+func (r PixKeyRepositoryDB) AddBank(bank *model.Bank) error {
+	err := r.DB.Create(bank).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r PixKeyRepositoryDB) AddAccount(account *model.Account) error {
+	err := r.DB.Create(account).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r PixKeyRepositoryDB) RegisterKey(pixKey *model.PixKey) (*model.PixKey, error) {
+	err := r.DB.Create(pixKey).Error
+	if err != nil {
+		return nil, err
+	}
+	return pixKey, nil
+}
+
+func (r PixKeyRepositoryDB) FindKeyByKind(key string, kind string) (*model.PixKey, error) {
+	var pixKey model.PixKey
+	r.DB.Preload("Account.Bank").First(&pixKey, "kind = ? and key = ?", kind, key)
+
+	if pixKey.ID == "" {
+		return nil, fmt.Errorf("no key was found")
+	}
+	return &pixKey, nil
+}
+
+func (r PixKeyRepositoryDB) FindAccount(id string) (*model.Account, error) {
+	var account model.Account
+	r.DB.Preload("Bank").First(&account, "id = ?", id)
+
+	if account.ID == "" {
+		return nil, fmt.Errorf("no account found")
+	}
+	return &account, nil
+}
+
+func (r PixKeyRepositoryDB) FindBank(id string) (*model.Bank, error) {
+	var bank model.Bank
+	r.DB.First(&bank, "id = ?", id)
+
+	if bank.ID == "" {
+		return nil, fmt.Errorf("no bank found")
+	}
+	return &bank, nil
+}
