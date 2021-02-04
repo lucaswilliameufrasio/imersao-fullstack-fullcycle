15f39d23869d6a031aaf7e351aaead9fe0a23ade
Lucas William <lucaswilliameufrasio@gmail.com>
2021-02-04 04:03:10 -0300

    feat: add PixService composition

--- codepix/app/grpc/server.go
+++ codepix/app/grpc/server.go
@@ -6,11 +6,20 @@ import (
 	"net"
 
 	"github.com/jinzhu/gorm"
+	"github.com/lucaswilliameufrasio/imersao/codepix-go/app/grpc/pb"
+	"github.com/lucaswilliameufrasio/imersao/codepix-go/app/usecase"
+	"github.com/lucaswilliameufrasio/imersao/codepix-go/infra/repository"
 	"google.golang.org/grpc"
 )
 
 func StartGrpcServer(database *gorm.DB, port int) {
 	grpcServer := grpc.NewServer()
+
+	pixRepository := repository.PixKeyRepositoryDB{DB: database}
+	pixUseCase := usecase.PixUseCase{PixKeyRepository: pixRepository}
+	pixGrpcService := NewPixGrpcService(pixUseCase)
+
+	pb.RegisterPixServiceServer(grpcServer, pixGrpcService)
 
 	address := fmt.Sprintf("0.0.0.0:%d", port)
 	listener, err := net.Listen("tcp", address)
